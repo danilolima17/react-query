da3459b4f28cf6ee45ff06a1eb3b82f54dca29e3
danilolima17 <danilodavite20@hotmail.com>
2023-07-24 14:56:33 -0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />;
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,63 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+
+type TodoProps = {
+  userId: number;
+  title: string;
+  id: number;
+};
+
+const fetchData = async () => {
+  try {
+    const result = await axios.get<TodoProps[]>('https://jsonplaceholder.typicode.com/todos');
+    return result.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+const Home: NextPage = () => {
+  const { data, isLoading } = useQuery<TodoProps[], Error>({
+    queryKey: ["todos"],
+    queryFn: fetchData,
+  });
+
+  const todos = data;
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>React Query</title>
+        <link rel="icon" href="" />
+      </Head>
+
+      <main className="relative flex w-full flex-1 flex-col items-center px-20 text-center">
+       
+        <div className="flex flex-col gap-y-4">
+          {isLoading ? (
+            <p>Carregando...</p>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+              <p className="text-2xl font-bold my-5">Products</p>
+              {todos?.map((todo) => (
+                <div
+                  key={todo.id}
+                  className="max-w-sm shadow-lg border p-4 border-gray-200 rounded-lg text-left"
+                >
+                  <p className="text-lg font-semibold">{todo.title}</p>
+                  <p className="text-gray-700">{`User ID: ${todo.userId}`}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
